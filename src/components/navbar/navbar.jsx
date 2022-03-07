dbf8d1e7e41813ae3ff4a2b567cd258e44973c3e
youngjun <juni2004c@gmail.com>
2022-03-08 02:28:58 +0900

    add navbar UI

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./navbar.module.css";
+
+const Navbar = (props) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoAndTitle}>
+        <img className={styles.logo} src="/assets/favicon.png" alt="" />
+        <h1 className={styles.title}>Youngjun</h1>
+      </div>
+      <ul className={styles.btns}>
+        <button className={styles.btn}>Home</button>
+        <button className={styles.btn}>About</button>
+        <button className={styles.btn}>Skills</button>
+        <button className={styles.btn}>My work</button>
+        <button className={styles.btn}>Contact</button>
+      </ul>
+    </header>
+  );
+};
+
+export default Navbar;
