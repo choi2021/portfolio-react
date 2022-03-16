777ea4ab0887382801e6f995d8b45a78e54d88e7
youngjun <juni2004c@gmail.com>
2022-03-17 04:17:32 +0900

    home page UI 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Navbar from "./components/navbar/navbar";
+import Home from "./pages/home/home";
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <Home></Home>
     </>
   );
 }
